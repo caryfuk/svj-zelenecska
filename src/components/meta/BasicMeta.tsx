bde0a5987506cee84ca6f5e1d8f2aed77ac15aaf
Pavel Moravec <pmoravec@ebay.com>
2021-08-31 21:41:48 +0200

    fix meta tags

--- src/components/meta/BasicMeta.tsx
+++ src/components/meta/BasicMeta.tsx
@@ -33,11 +33,7 @@ export default function BasicMeta({
         }
       />
       {author ? <meta name="author" content={author} /> : null}
-      <meta name="theme-color" content="#1A1" />
       <link rel="canonical" href={config.base_url + url} />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
     </Head>
   );
 }
